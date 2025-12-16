cb2ac23249a12546d069f9d77fdf5794baff7bd2
Matute95 <matias.01001101@gmail.com>
2025-12-16 15:07:43 -0400

    next.config

--- next.config.js
+++ next.config.js
@@ -1,38 +1,39 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	compiler: {
-		styledComponents: true,
-	},
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
+  reactStrictMode: true,
 
-	webpack: (config, options) => {
+  images: {
+    unoptimized: true,
+  },
 
-		config.module.rules = config.module.rules.map(rule => {
-		if (rule.test && rule.test.test('.svg')) {
-			return { ...rule, exclude: /\.svg$/ };
-		}
-			return rule;
-		});
-	
-		config.module.rules.push({
-			test: /\.svg$/i,
-			issuer: /\.[jt]sx?$/,
-			use: ['@svgr/webpack'],
-		});
+  compiler: {
+    styledComponents: true,
+  },
 
-		config.module.rules.push({
-			test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
-			type: "asset/resource",
-			generator: {
-				filename: "static/chunks/[path][name].[hash][ext]",
-			},
-		});
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 
-		return config;
-	},
+  webpack: (config) => {
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.test && rule.test.test('.svg')) {
+        return { ...rule, exclude: /\.svg$/ };
+      }
+      return rule;
+    });
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    config.module.rules.push({
+      test: /\.(ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
+      type: "asset/resource",
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
